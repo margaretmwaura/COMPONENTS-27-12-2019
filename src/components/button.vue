052680dd4bcf569c4ec85bda8b271177c540ff3d
Margaret Mwaura <mwauramargaret1@gmail.com>
2019-12-27 08:14:52 +0300

    using components

---
+++ src/components/button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button>Gal is working hard she is trying </button>
+</template>
+
+<script>
+    export default
+    {
+        name: "buttonmine"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/pratice.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div>
+    <h1>I am Maggie , I am really trying man</h1>
+    <buttonmine></buttonmine>
+    </div>
+</template>
+
+
+<script>
+    import buttonmine from './button'
+    export default
+    {
+        name: "pratice",
+        components: {
+           buttonmine
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
