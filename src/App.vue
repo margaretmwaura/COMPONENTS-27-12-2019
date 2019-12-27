b0f63a7661209cd44fa12d9872949a7d86814380
Margaret Mwaura <mwauramargaret1@gmail.com>
2019-12-27 08:26:18 +0300

    proper use of props

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,34 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <pratice></pratice>
+    <listusingprops v-bind:artists = "artists"/>
   </div>
 </template>
 
 <script>
   import pratice from './components/pratice'
-
+  import listusingprops from './components/listusingprops'
 export default {
   name: 'app',
   components: {
-    pratice
+    pratice,
+    listusingprops
+  },
+  data (){
+    return {
+      artists: [
+        {name: 'Davido', genre: 'afrobeats', country: 'Nigeria'},
+        {name: 'Burna Boy', genre: 'afrobeats', country: 'Nigeria'},
+        {name: 'AKA', genre: 'hiphop', country: 'South-Africa'},
+        {name: 'Sarkodie', genre: 'hiphop', country: 'Ghana'},
+        {name: 'Stormzy', genre: 'hiphop', country: 'United Kingdom'},
+        {name: 'Lil Nas', genre: 'Country', country: 'United States'},
+        {name: 'Nasty C', genre: 'hiphop', country: 'South-Africa'},
+        {name: 'Shatta-walle', genre: 'Reagae', country: 'Ghana'},
+        {name: 'Khalid', genre: 'pop', country: 'United States'},
+        {name: 'ed-Sheran', genre: 'pop', country: 'United Kingdom'}
+      ]
+    }
   }
 }
 </script>
